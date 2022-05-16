e788e5677cfa58a0585424f471caff3fbd21c2df
antongotech <105588893+antongotech@users.noreply.github.com>
2022-05-16 14:55:24 +0300

    Infinite scroll implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import {Box, Container, List, ListItem, ListItemText, TextField, Typography} from '@mui/material'
+import React, {ChangeEvent, useState, useRef, useCallback} from 'react'
+import useSearch from './useSearch'
 
 function App() {
+    const [text, setText] = useState<string>('')
+    const [page, setPage] = useState<number>(1)
+
+    const observer: any = useRef()
+    const {books, hasMore, loading, error} = useSearch(text, page)
+
+    const lastInstanceRef = useCallback((node: any) => {
+        if (loading) return
+        if (observer.current) observer.current.disconnect()
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && hasMore) setPage(prevState => prevState + 1)
+        })
+        if (node) observer.current.observe(node)
+    }, [loading, hasMore])
+
+    const handleSearch = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setText(e.target.value)
+        setPage(1)
+    }
     return (
-        <div className="App">
-            App
-        </div>
+        <Container maxWidth='lg'>
+            <Box display='flex' justifyContent='center' alignItems='center' py={3}>
+                <TextField sx={{minWidth: 300}} id="standard-basic" label="Search for books..." variant="standard"
+                           value={text}
+                           onChange={(e) => handleSearch(e)}/>
+            </Box>
+            {
+                error && <Typography variant='body2' color='text.secondary'>Error. Please reload the page</Typography>
+            }
+            {
+                !error && books.length > 0 && <List>
+                    {books.map((b, i) => {
+                        if (books.length === i + 1) {
+                            return <ListItem ref={lastInstanceRef} key={b + i}><ListItemText
+                                primary={`${i + 1}. ${b}`}/></ListItem>
+                        } else {
+                            return <ListItem key={b + i}><ListItemText primary={`${i + 1}. ${b}`}/></ListItem>
+                        }
+                    })}
+                </List>
+            }
+            {
+                loading && <Typography variant='body2' color='text.secondary'>Loading...</Typography>
+            }
+            {
+                !loading && !books.length &&
+                <Typography variant='body2' color='text.secondary'>Nothing has been found</Typography>
+            }
+
+        </Container>
     )
 }
 
---
+++ src/useSearch.tsx
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
+
+interface ISearch {
+    query: string
+    page: number
+}
+
+const UseSearch = (query: string, page: number) => {
+    const [loading, setLoading] = useState<boolean>(true)
+    const [error, setError] = useState<boolean>(false)
+    const [books, setBooks] = useState<any[]>([])
+    const [hasMore, setHasMore] = useState<boolean>(false)
+
+    useEffect(() => {
+        setBooks([])
+    },[query])
+
+    useEffect(() => {
+        setLoading(true)
+        setError(false)
+        let cancel: Function
+        axios({
+            method: 'GET',
+            url: 'http://openlibrary.org/search.json',
+            params: {q: query, page: page},
+            cancelToken: new axios.CancelToken((c) => cancel = c)
+        }).then((res) => {
+            setBooks(prevState => [...prevState, ...res.data.docs.map((b:any) => b.title)])
+            setHasMore(res.data.docs.length > 0)
+            setLoading(false)
+        }).catch(e => {
+            if (axios.isCancel(e)) return
+            setError(true)
+        })
+
+        return () => cancel()
+    }, [query, page])
+
+    return {loading, error, books, hasMore}
+}
+
+export default UseSearch
