e471da1c6c91b7a62a6966a6d04de82b1b4ed77f
antongotech <105588893+antongotech@users.noreply.github.com>
2022-05-31 18:27:11 +0300

    Logic fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,30 @@ import InfiniteScroll from './InfiniteScroll/InfiniteScroll'
 
 interface IRow {
     index: number
-    style: React.CSSProperties | undefined
+    style?: React.CSSProperties | undefined
+    text?: string
 }
 
-const Row: React.FC<IRow> = ({index, style}) => {
-    const overridenStyles: CSSProperties = {...style, textAlign: 'center', lineHeight: '80px'}
+const Row: React.FC<IRow> = ({index, style, text}) => {
+    const overridenStyles: CSSProperties = {...style, textAlign: 'center', lineHeight: '50px'}
     return (
-        <div style={overridenStyles}>Row {index}</div>
+        <div style={overridenStyles}>Row {index}: {text}</div>
     )
 }
 
 function App() {
 
+    const its = [{id: 1, text: 'Hey 1'}, {id: 2, text: 'Hey 2'}, {id: 3, text: 'Hey 3'}, {id: 4, text: 'Hey 4'},
+        {id: 5, text: 'Hey 5'}, {id: 6, text: 'Hey 6'}, {id: 7, text: 'Hey 7'}, {id: 8, text: 'Hey 8'},
+        {id: 9, text: 'Hey 9'}, {id: 10, text: 'Hey 10'}, {id: 11, text: 'Hey 11'}, {id: 12, text: 'Hey 12'},
+        {id: 13, text: 'Hey 13'}, {id: 14, text: 'Hey 14'}, {id: 15, text: 'Hey 15'}, {id: 16, text: 'Hey 16'}]
+
     return (
         <>
             <h3 style={{textAlign: 'center'}}>Infinite scroll</h3>
-            <InfiniteScroll height={550} width='100%' amount={100} Item={Row} rendered={10} inFocus={7}/>
+            <InfiniteScroll style={{width: '100%', height: 250}}>
+                {its.map(({id, text}) => <Row key={id} index={id} text={text}/>)}
+            </InfiniteScroll>
         </>
     )
 }
--- src/InfiniteScroll/InfiniteScroll.tsx
+++ src/InfiniteScroll/InfiniteScroll.tsx
@@ -1,43 +1,33 @@
-import React, {useEffect, useMemo, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 interface IInfiniteScroll {
-    height: number
-    width: number | string
-    amount: number
-    Item: any
-    rendered: number
-    inFocus: number
+    style: React.CSSProperties
+    children: Array<any>
 }
 
-const InfiniteScroll: React.FC<IInfiniteScroll> = ({height, width, amount, Item, rendered, inFocus}) => {
-    const inputRef = React.useRef<HTMLInputElement>(null)
+const InfiniteScroll: React.FC<IInfiniteScroll> = ({style, children}) => {
+    const containerRef = React.useRef<HTMLInputElement>(null)
     const [currentPosition, setCurrentPosition] = useState<number>(0)
-    const [index, setIndex] = useState<number>(0)
+    const [renderIndex, setRenderIndex] = useState<number>(0)
 
-    const itemHeight = useMemo(() => height / inFocus, [height])
+    const rendered = 10
+    const itemHeight = 50
 
-    const items = useMemo(() => {
-        let temp = []
-        for (let i = amount; i > 0; i--) {
-            temp.push(
-                <Item
-                    key={i}
-                    index={i}
-                    style={{
-                        width: '100%',
-                        height: itemHeight,
-                        background: i % 2 === 0 ? 'lightgray' : 'white',
-                        position: 'absolute',
-                        left: 0,
-                        top: i === 1 ? 0 : i * itemHeight - itemHeight
-                    }}
-                />
-            )
-        }
-        return temp.reverse()
-    }, [amount, Item, itemHeight])
+    const items = children.map((child, i) =>
+        <div
+            key={child.props.index}
+            style={{
+                width: '100%',
+                height: itemHeight,
+                background: i % 2 === 0 ? 'lightgray' : 'white',
+                position: 'absolute',
+                left: 0,
+                top: i === 0 ? 0 : i * itemHeight
+            }}>
+            {child}
+        </div>)
 
-    const [displayed, setDisplayed] = useState(() => {
+    const [displayedItems, setDisplayedItems] = useState(() => {
         if (items.length > rendered) {
             return [...items.slice(0, rendered)]
         } else return items
@@ -45,34 +35,33 @@ const InfiniteScroll: React.FC<IInfiniteScroll> = ({height, width, amount, Item,
 
     // Attaches scroll event to the list, event handles scroll direction and position
     useEffect(() => {
-        const onScroll = () => setCurrentPosition(!inputRef.current ? 0 : inputRef.current.scrollTop)
+        const onScroll = () => setCurrentPosition(!containerRef.current ? 0 : containerRef.current.scrollTop)
 
-        inputRef.current?.addEventListener('scroll', onScroll)
+        containerRef.current?.addEventListener('scroll', onScroll)
 
-        return () => inputRef.current?.removeEventListener('scroll', onScroll)
+        return () => containerRef.current?.removeEventListener('scroll', onScroll)
     }, [])
 
-    // Change last index of visible items according to amount of scrolled pixels
+    // Change last renderIndex of visible items according to amount of scrolled pixels
     useEffect(() => {
-        setIndex(Math.floor(currentPosition / itemHeight))
+        setRenderIndex(Math.floor(currentPosition / itemHeight))
     }, [currentPosition])
 
-    // Add element at the end / beginning of list according to index
+    // Add element at the end / beginning of list according to renderIndex
     useEffect(() => {
-        setDisplayed(items.slice(index, rendered + index))
-    }, [index])
+        setDisplayedItems(items.slice(renderIndex, rendered + renderIndex))
+    }, [renderIndex])
 
     return (
-        <div ref={inputRef}
+        <div ref={containerRef}
              style={{
-                 width: width,
-                 height: height,
+                 ...style,
                  border: '1px solid gray',
                  overflowY: 'scroll',
                  position: 'relative'
              }}
         >
-            {displayed}
+            {displayedItems}
             <div style={{height: items.length * itemHeight}}/>
         </div>
     )
